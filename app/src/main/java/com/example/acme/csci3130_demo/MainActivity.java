cd8a27b891709c8179ad92d9c4168e33be1bf045
ali.rizvi <sy269533@dal.ca>
2018-01-24 10:58:11 -0400

    Commiting assignment

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.acme.csci3130_demo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+        EditText in = (EditText) findViewById(R.id.in);
+        TextView out = (TextView) findViewById(R.id.out);
+        out.setText(in.getText());
+}
+        });
     }
+
 }
